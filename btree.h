18afc715a7a12935d252105d1edc3ffd8409346c
lindolfo1 <cuentas@feptec.com>
2023-04-06 11:03:07 -0500

    modified:   btree.h

---
+++ btree.h
@@ -0,0 +1,49 @@
+struct node {
+    int num;
+    node *left;
+    node *right;
+
+    node() {
+        num = 0;
+        left = nullptr;
+        right = nullptr;
+    }
+
+    node(int n) { num = n; }
+};
+
+class btree {
+   private:
+    node *root;
+
+   public:
+    btree() { root = nullptr; }
+
+    bool empty() { return root == nullptr; }
+
+    bool insert(int num) {
+        if (empty()) {
+            node *tmp = new node(num);
+            root = tmp;
+            return true;
+        }
+        return insertR(num, root);
+    }
+
+    bool insertR(int num, node *r) {
+        if (r != nullptr) {
+            if (num == r->num) return false;
+            if (num < r->num) {
+                if (r->left == nullptr)
+                    r->left = new node(num);
+                else
+                    return insertR(num, r->left);
+            } else {
+                if (r->right == nullptr)
+                    r->right = new node(num);
+                else
+                    return insertR(num, r->right);
+            }
+        }
+    }
+};
