08c3d5b2f249604cda47fdb0d75efd32b17819a1
lindolfo1 <cuentas@feptec.com>
2023-04-06 12:51:29 -0500

    new file:   .vscode/settings.json 	new file:   .vscode/tasks.json 	new file:   main 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
-#include <iostream>
-#include <fstream>
-#include <string>
 #include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
 #include "ArgumentManager.h"
 #include "btree.h"
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-
+int main(int argc, char *argv[]) {
+    btree tree = btree();
+    
     return 0;
 }
