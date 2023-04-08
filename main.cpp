515a88242c5b28425d30c177b9c0480aae459117
Alan Johnson <alanjcd2004@gmail.com>
2023-04-08 15:01:11 -0500

    completed preorder function

--- main.cpp
+++ main.cpp
@@ -8,8 +8,18 @@
 #include "btree.h"
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     btree tree = btree();
-    
+    tree.insert(9);
+    tree.insert(6);
+    tree.insert(3);
+    tree.insert(2);
+    tree.insert(5);
+    tree.insert(4);
+    tree.insert(1);
+
+    string temp = "[x";
+    tree.preorder(tree.getRoot(), temp, false);
     return 0;
 }
